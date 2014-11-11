8cf3f75e29e954ef3faa5799c0417c61c7d38104
Osamu Hirose <ohirose.univ@gmail.com>
2014-11-11 17:39:48 +0900

    "read_t.c" was slightly modified and "read" was updated.

--- misc/read_t.c
+++ misc/read_t.c
@@ -3,14 +3,16 @@
 
 int main(int argc, char** argv){
 
-  int s,k,K,t,end,*buf,l,m,n,L,M,N;
+  int s,k,K,t,end,*buf,l,m,n,L,M,N,flag;
   FILE *fp  = fopen(argv[1],"rb");
 
+  flag= (argc>2)?1:0;
   fseek(fp,0,SEEK_END);end=ftell(fp);fseek(fp,0,SEEK_SET); 
   buf = malloc((1+end)*sizeof(int)); 
 
   fread(buf,sizeof(int),8,fp); 
-  printf("Resolution:   %3dx%3dx%2d\n",buf[0],buf[1],buf[2]);
+  if(!flag) printf("Resolution:   %3dx%3dx%2d\n",buf[0],buf[1],buf[2]);
+  if( flag) printf("Resolution:   %3dx%3dx%2d\n",buf[1],buf[0],buf[2]);
   printf("Tracker size: %3dx%3dx%2d\n",buf[4],buf[5],buf[6]);
   printf("#Frames:      %3d\n",        buf[3]);
   printf("Cutoff:       %3d\n\n",      buf[7]);
@@ -23,8 +25,8 @@ int main(int argc, char** argv){
 
     fread(buf,sizeof(int),4*K,fp); 
     for(k=0;k<K;k++){l=buf[4*k];m=buf[4*k+1];n=buf[4*k+2];s=buf[4*k+3];
-      if(argc==2) printf("%4d%4d%4d%4d\n",  l,  m,  n,s);
-      if(argc> 2) printf("%4d%4d%4d%4d\n",M-m,l+1,N-n,s);
+      if(!flag) printf("%4d%4d%4d%4d\n",  l,  m,  n,s);
+      if( flag) printf("%4d%4d%4d%4d\n",M-m,l+1,N-n,s);
     } printf("\n");
   }
 
